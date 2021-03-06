59cdddf090468456a310d5f07170eac12e7dda6e
root <root@localhost.localdomain>
2021-03-06 13:19:35 +0330

    final changes 1

--- new.py
+++ new.py
@@ -3,17 +3,21 @@ import ipaddress
 import threading
 import subprocess
 ipl=[]
-def ipcheck(x):
-    ipl.append(x)
-    print ('======={CHECKING}====>>>'+x)
-    res=subprocess.call(['ping '+x+' -c 1 -W 1'],shell=True,universal_newlines=True,stdout=subprocess.PIPE,)
-    if res ==0:
-        print ('=========='+' Host is   up '+'==========(@_@)')
-    elif res < 2:
-        print ('=========='+' Host is down '+'==========(X_X)')
-for ip in ipl:
-    t=threading.Thread(target=ipcheck(ip))
 dbcon= MySQLdb.connect(user='pedram',password='321321',host='localhost',database='pedram')
 cur=dbcon.cursor()
-cur.execute('select ip from ip_ping ip')
-for dbn in cur:
+class pedram:
+    def ipcheck(x):
+        print ('======={CHECKING}====>>>'+x)
+        res=subprocess.call(['ping '+x+' -c 1 -W 1'],shell=True,universal_newlines=True,stdout=subprocess.PIPE,)
+        if res ==0:
+            print ('=========='+' Host is   up '+'==========(@_@)')
+        elif res < 2:
+            print ('=========='+' Host is down '+'==========(X_X)')
+    def start():
+        for ip in ipl:
+            t=threading.Thread(target=pedram.ipcheck(ip))
+cur.execute('select ip from ip_ping')
+for ipn in cur:
+    d=list(ipn)
+    ipl.append(d[0])
+pedram.start()
